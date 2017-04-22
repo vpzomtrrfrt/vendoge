6a29d5164c49cc162a209e1d8cdc504017ab4af4
Colin Reeder <vpzomtrrfrt@gmail.com>
2017-04-21 19:16:33 -0600

    Various improvements

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ var payments = new (require('coinpayments'))({
 	key: process.env.CONSUMER_KEY,
 	secret: process.env.CONSUMER_SECRET
 });
+var https = require('https');
+
+var COST = 200;
 
 function parseDBURL(uri) {
 	var params = require('url').parse(uri);
@@ -34,34 +37,35 @@ app.use("/buy", function(req, res, next) {
 	Q.ninvoke(payments, "createTransaction", {
 		currency1: 'DOGE',
 		currency2: 'DOGE',
-		amount: 20,
+		amount: COST,
 		buyer_email: req.body.email
 	})
-	.then(function(trans) {
-		var id = hat(16, 4);
-		return db.connect()
-		.then(function(client) {
-			return client.query("INSERT INTO payments (timestamp, id, coinpayments_id) VALUES (localtimestamp, $1, $2)", [id, trans.txn_id])
-			.then(function(result) {
-				client.release();
-			}, function(err) {
-				client.release();
-				throw err;
-			})
-			.then(function() {
-				res.writeHead(200, {"Content-type": "text/html"});
-				res.write("<!DOCTYPE html><html><head><title>Payment link stuff</title></head><body>");
-				res.write('<a href="'+trans.status_url+'" target="_blank">Link</a>');
-				res.write('<br/><img src="'+trans.qrcode_url+'" />');
-				res.write('<br/>Code: '+id);
-				res.write('</body></html>');
-				res.end();
-			});
+		.then(function(trans) {
+			var id = hat(16, 4);
+			return db.connect()
+				.then(function(client) {
+					return client.query("INSERT INTO payments (timestamp, id, coinpayments_id) VALUES (localtimestamp, $1, $2)", [id, trans.txn_id])
+						.then(function(result) {
+							client.release();
+						}, function(err) {
+							client.release();
+							throw err;
+						})
+						.then(function() {
+							res.writeHead(200, {"Content-type": "text/html"});
+							res.write("<!DOCTYPE html><html><head><title>Payment link stuff</title></head><body>");
+							res.write('<a href="'+trans.status_url+'" target="_blank">Link</a>');
+							res.write('<br/><img src="'+trans.qrcode_url+'" />');
+							res.write('<br/>Code: '+id);
+							res.write('</body></html>');
+							res.end();
+						});
+				});
+		})
+		.catch(function(err) {
+			console.error(err);
+			next(err);
 		});
-	})
-	.catch(function(err) {
-		next(err);
-	});
 });
 
 app.use("/redeem", function(req, res, next) {
@@ -71,28 +75,66 @@ app.use("/redeem", function(req, res, next) {
 	}
 	else {
 		db.connect()
-		.then(function(client) {
-			return client.query("SELECT coinpayments_id FROM payments WHERE id = $1", [spl[1]])
-			.then(function(result) {
-				client.release();
-				if(result.rows.length < 1) {
-					throw "No such ID";
-				}
-				Q.ninvoke(payments, "getTx", result.rows[0].coinpayments_id)
-					.then(function(trans) {
-						if(trans.status < 100) {
-							res.writeHead(403, {"Content-type": "text/plain"});
-							res.end(trans.status_text);
+			.then(function(client) {
+				return client.query("SELECT coinpayments_id FROM payments WHERE id = $1", [spl[1]])
+					.then(function(result) {
+						if(result.rows.length < 1) {
+							throw "Invalid code";
 						}
-						else {
-							res.end("OK");
-						}
+						return Q.ninvoke(payments, "getTx", result.rows[0].coinpayments_id)
+							.then(function(trans) {
+								if(trans.status >= 100) {
+									return "coinpayments says yes";
+								}
+								return new Promise(function(resolve, reject) {
+									console.log("getting");
+									https.get("https://chain.so/api/v2/get_address_balance/DOGE/"+trans.payment_address, function(res) {
+										console.log("res achieved");
+										if(res.statusCode !== 200) {
+											reject(trans.status_text);
+											return;
+										}
+										var data = "";
+										res.on('data', function(chunk) {
+											data += chunk;
+										});
+										res.on('end', function() {
+											console.log("end");
+											var j = JSON.parse(data);
+											if(j.status != "success") {
+												reject(trans.status_text);
+												console.error(j);
+												return;
+											}
+											if(parseFloat(j.data.confirmed_balance) == COST) {
+												resolve("chain.so says yes");
+												return;
+											}
+											reject(trans.status_text);
+											console.error(j);
+										});
+										res.on('error', reject);
+									});
+								});
+							})
+							.then(function() {
+								return client.query("DELETE FROM payments WHERE id = $1", [spl[1]])
+									.then(function() {
+										res.end("OK");
+									});
+							}, function(err) {
+								res.writeHead(403, {"Content-type": "text/plain"});
+								res.end(err);
+							});
 					});
 			}, function(err) {
 				client.release();
 				throw err;
+			})
+			.catch(function(e) {
+				res.writeHead(403, {"Content-type": "text/plain"});
+				res.end(e);
 			});
-		});
 	}
 });
 
